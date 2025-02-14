7ca1ce5e94226bd8991a848f9a680a574bbe9de2
Montange <richardkravenbiscante13@gmail.com>
2025-02-14 15:46:42 +0800

    added playButton and exitButton listeners

--- OSHang.java
+++ OSHang.java
@@ -1,7 +1,7 @@
-package imagecompression2;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OSHang {
     private JFrame frame;
@@ -14,16 +14,31 @@ public class OSHang {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
 
-        ImageIcon serverRoom = new ImageIcon("C:\\Users\\ACER\\Downloads\\OSHang\\OSHang GUI\\serverRoom.gif");
+        ImageIcon serverRoom = new ImageIcon("OSHang GUI/serverRoom.gif");
         JLabel serverRoomLabel = new JLabel(serverRoom);
         serverRoomLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
         frame.setContentPane(serverRoomLabel);
         serverRoomLabel.setLayout(null);
 
-        JButton playButton = createImageButton("C:\\Users\\ACER\\Downloads\\OSHang\\OSHang GUI\\playButton.png", 300, 150, 155, 50);
-        JButton settingsButton = createImageButton("C:\\Users\\ACER\\Downloads\\OSHang\\OSHang GUI\\settingsButton.png", 300, 220, 155, 50);
-        JButton exitButton = createImageButton("C:\\Users\\ACER\\Downloads\\OSHang\\OSHang GUI\\exitButton.png", 300, 290, 155, 50);
-        JButton aboutButton = createImageButton("C:\\Users\\ACER\\Downloads\\OSHang\\OSHang GUI\\aboutButton.png", 700, 410, 50, 50);
+        JButton playButton = createImageButton("OSHang GUI/playButton.png", 300, 150, 155, 50);
+        JButton settingsButton = createImageButton("OSHang GUI/settingsButton.png", 300, 220, 155, 50);
+        JButton exitButton = createImageButton("OSHang GUI/exitButton.png", 300, 290, 155, 50);
+        JButton aboutButton = createImageButton("OSHang GUI/aboutButton.png", 700, 410, 50, 50);
+
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // Close current window
+                new playButton(); // Open new window
+            }
+        });
+        
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Terminate the application
+            }
+        });
 
         serverRoomLabel.add(playButton);
         serverRoomLabel.add(settingsButton);
@@ -35,7 +50,6 @@ public class OSHang {
 
     private JButton createImageButton(String imagePath, int x, int y, int width, int height) {
         ImageIcon icon = new ImageIcon(imagePath);
-    
         Image scaledImage = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
 
@@ -44,10 +58,9 @@ public class OSHang {
         button.setFocusPainted(false);
         button.setBorderPainted(false);
         button.setContentAreaFilled(false);
-    
+
         return button;
     }
-
 
     public static void main(String[] args) {
         new OSHang();
